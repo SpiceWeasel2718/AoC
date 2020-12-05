ccf8a3be79d46a9ea691c481810c5b5a9b457442
SpiceWeasel2718 <49452152+SpiceWeasel2718@users.noreply.github.com>
2020-12-05 01:02:37 -0600

    switched method for p2 so it actually works

--- 2020/AoC2020_day05.py
+++ 2020/AoC2020_day05.py
@@ -17,7 +17,9 @@ def part1(input_text):
     return highest
 
 
-def part2(input_text, result):
+def part2(input_text):
+    unoccupied = set(range(1024))
+
     for seat in input_text.splitlines():
         n = 0
 
@@ -29,21 +31,22 @@ def part2(input_text, result):
             n <<= 1
             n += (c == 'R')
 
-        result ^= n
+        unoccupied.remove(n)
 
-    return result
+    for seat_num in unoccupied:
+        if not seat_num-1 in unoccupied and not seat_num+1 in unoccupied:
+            return seat_num
 
 
 if __name__ == '__main__':
     with open('./2020/input_files/AoC2020_day05_input.txt') as f:
         input_text = f.read()
 
-    
-    print('part 1:', p1 := part1(input_text))
-    print('part 2:', part2(input_text, p1))
+    print('part 1:', part1(input_text))
+    print('part 2:', part2(input_text))
 
     import timeit
     p1_timer = timeit.Timer('part1(input_text)', globals=globals())
-    p2_timer = timeit.Timer('part2(input_text, p1)', globals=globals())
+    p2_timer = timeit.Timer('part2(input_text)', globals=globals())
     # print(f'part1 took at least {min(p1_timer.repeat(number=1)) :.5f} seconds')
     # print(f'part2 took at least {min(p2_timer.repeat(number=1)) :.5f} seconds')
