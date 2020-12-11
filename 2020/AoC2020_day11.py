58ad75d5e04f20ab1a8d2452ce935cc034a44c20
SpiceWeasel2718 <49452152+SpiceWeasel2718@users.noreply.github.com>
2020-12-11 03:09:43 -0600

    day 11 post-cleanup

--- 2020/AoC2020_day11.py
+++ 2020/AoC2020_day11.py
@@ -1,66 +1,83 @@
 def part1(input_text):
-    mask = [[(c == 'L') for c in line] for line in input_text.splitlines()]
-    rows = len(mask)
-    cols = len(mask[0])
-    area = [[0]*cols for __ in range(rows)]
-    old_area = []
+    old_area = {}
+    area = {}
+
+    for r, line in enumerate(input_text.splitlines()):
+        for c, char in enumerate(line):
+            if char == 'L':
+                area[(r, c)] = 0
 
     nbr_offset = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-    while old_area != area:
-        old_area, area = area, [[0]*cols for __ in range(rows)]
-        
-        for r in range(rows):
-            for c in range(cols):
-                if not mask[r][c]:
-                    continue
-                count = 0
-                for ro, co in nbr_offset:
-                    if 0 <= (rr := r + ro) < rows and 0 <= (cc := c + co) < cols:
-                        count += old_area[rr][cc]
-                if not old_area[r][c] and count == 0:
-                    area[r][c] = 1
-                elif old_area[r][c] and count >= 4:
-                    area[r][c] = 0
-                else:
-                    area[r][c] = old_area[r][c]
+    neighbors = {}
+    for r, c in area:
+        neighbors[(r, c)] = [(r+ro, c+co) for ro, co in nbr_offset if (r+ro, c+co) in area]
     
-    return sum(sum(row) for row in area)
-        
+    changed = True
+
+    while changed:
+        old_area, area = area, {}
+        changed = False
+
+        for pos in old_area:
+            count = sum(old_area[nbr] for nbr in neighbors[pos])
+            if count == 0 and not old_area[pos]:
+                area[pos] = 1
+                changed = True
+            elif count >= 4 and old_area[pos]:
+                area[pos] = 0
+                changed = True
+            else:
+                area[pos] = old_area[pos]
+    
+    return sum(area.values())
 
 
 def part2(input_text):
-    mask = [[(c == 'L') for c in line] for line in input_text.splitlines()]
-    rows = len(mask)
-    cols = len(mask[0])
-    area = [[0]*cols for __ in range(rows)]
-    old_area = []
+    old_area = {}
+    area = {}
+    all_seats = set()
+
+    for r, line in enumerate(input_list := input_text.splitlines()):
+        for c, char in enumerate(line):
+            all_seats.add((r, c))
+            if char == 'L':
+                area[(r, c)] = 0
+
+    rows, cols = len(input_list), len(input_list[0])
 
     nbr_offset = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-    while old_area != area:
-        old_area, area = area, [[0]*cols for __ in range(rows)]
-        
-        for r in range(rows):
-            for c in range(cols):
-                if not mask[r][c]:
-                    continue
-                count = 0
-                for ro, co in nbr_offset:
-                    rr, cc = r + ro, c + co
-                    while (0 <= rr < rows and 0 <= cc < cols) and not mask[rr][cc]:
-                        rr += ro
-                        cc += co
-                    if (0 <= rr < rows and 0 <= cc < cols):
-                        count += old_area[rr][cc]
-                if not old_area[r][c] and count == 0:
-                    area[r][c] = 1
-                elif old_area[r][c] and count >= 5:
-                    area[r][c] = 0
-                else:
-                    area[r][c] = old_area[r][c]
+    neighbors = {}
     
-    return sum(sum(row) for row in area)
+    for r, c in area:
+        neighbors[(r, c)] = []
+        for ro, co in nbr_offset:
+            rr, cc = r + ro, c + co
+            while (rr, cc) not in area:
+                rr += ro
+                cc += co
+                if (rr, cc) not in all_seats:
+                    break
+            else:
+                neighbors[(r, c)].append((rr, cc))
+
+    changed = True
+
+    while changed:
+        old_area, area = area, {}
+        changed = False
+        
+        for pos in old_area:
+            count = sum(old_area[nbr] for nbr in neighbors[pos])
+            if count == 0 and not old_area[pos]:
+                area[pos] = 1
+                changed = True
+            elif count >= 5 and old_area[pos]:
+                area[pos] = 0
+                changed = True
+            else:
+                area[pos] = old_area[pos]
+    
+    return sum(area.values())
 
 
 if __name__ == '__main__':
@@ -70,7 +87,7 @@ if __name__ == '__main__':
     print('part 1:', part1(input_text))
     print('part 2:', part2(input_text))
 
-    time_execution = 0
+    time_execution = 1
 
     if time_execution:
         import timeit
