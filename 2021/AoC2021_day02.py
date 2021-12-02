f93daf1e2a1c44cb49cebb733a36a855785b1acb
SpiceWeasel2718 <49452152+SpiceWeasel2718@users.noreply.github.com>
2021-12-01 23:16:52 -0600

    day 2

---
+++ 2021/AoC2021_day02.py
@@ -0,0 +1,51 @@
+def part1(input_text):
+    depth = 0
+    horiz = 0
+
+    for line in input_text.splitlines():
+        d, n = line.split()
+
+        if d.startswith('d'):
+            depth += int(n)
+        elif d.startswith('u'):
+            depth -= int(n)
+        else:
+            horiz += int(n)
+    
+    return horiz * depth
+
+
+def part2(input_text):
+    depth = 0
+    horiz = 0
+    aim = 0
+
+    for line in input_text.splitlines():
+        d, n = line.split()
+
+        if d.startswith('d'):
+            aim += int(n)
+        elif d.startswith('u'):
+            aim -= int(n)
+        else:
+            horiz += int(n)
+            depth += aim * int(n)
+    
+    return horiz * depth
+
+
+if __name__ == '__main__':
+    with open('./input_files/AoC2021_day02_input.txt') as f:
+        input_text = f.read()
+
+    print('part 1:', part1(input_text))
+    print('part 2:', part2(input_text))
+
+    time_execution = 0
+
+    if time_execution:
+        import timeit
+        for part in ['part1', 'part2']:
+            timer = timeit.Timer(f'{part}(input_text)', globals=globals())
+            n, time = timer.autorange()
+            print(f'{part} took {time/n:.5f} seconds on average when run {n} times')
